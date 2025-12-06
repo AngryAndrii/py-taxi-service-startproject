a6701393ea8c13e267b64005ae61c1127ddfcb24
Andrii Chychkan <mywork1728@gmail>
2025-12-06 21:12:32 +0200

    finished

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from taxi.models import (Driver, Car, Manufacturer)
+
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("license_number",)
+    fieldsets = (UserAdmin.fieldsets +
+                 (("additional info", {"fields": ("license_number",)}),))
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {"fields": ("license_number",)},
+        ),
+    )
+
+
+@admin.register(Car)
+class CarAdmin(ModelAdmin):
+    list_filter = ("manufacturer",)
+    search_fields = ("model",)
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(ModelAdmin):
+    list_display = ModelAdmin.list_display + ("name", "country")
--- taxi/models.py
+++ taxi/models.py
@@ -12,12 +12,15 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, related_name="car"
+        settings.AUTH_USER_MODEL, related_name="cars"
     )
 
 
